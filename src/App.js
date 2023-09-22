8a6aaca70360ccf84154146c6c61345306e00e28
reneekaandorp <127418767+reneekaandorp@users.noreply.github.com>
2023-09-22 10:16:11 +0200

    imported weather app from Codesandbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import axios from "axios";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(props) {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    console.log(response.data);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <input type="submit" value="Search" />
+    </form>
   );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}</li>
+          <li>Wind: {Math.round(weather.wind)}</li>
+          <li>
+            {" "}
+            <img src={weather.icon} alt={weather.description} />{" "}
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Weather from "./Weather";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <Weather city="New York" />
+    <App city="Paris" />
   </React.StrictMode>
 );
 
